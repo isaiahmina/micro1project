687ef145b9940a29c852cef67e31e2863a3d788b
isao <38514030+isao21@users.noreply.github.com>
2018-04-19 15:57:17 -1000

    added data control

--- processor.java
+++ processor.java
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package micro1project;
+package ProjectCSCI;
 
 /**
  *
@@ -21,7 +21,7 @@ public class Processor /*extends memory?*/ {
         PC=0;
         IR=0;
     }
-    public boolean step(){  //returns true if encounters halt command (0)
+    public boolean step() throws Exception{  //returns true if encounters halt command (0)
          IR = memory.read(PC++);
          runCommand(IR);
 //        if(command==0)return true;
@@ -43,20 +43,188 @@ public class Processor /*extends memory?*/ {
     public void setMemory(Memory memory){
         this.memory = memory;
     }
-    private void runCommand(int command){
+    
+    /****************************data control****************/
+    private void load (int regA, int regB ){
+        reg[regA] = memory.cell[reg[regB]];
+    }
+    
+    private void loadc (int regA){
+        reg[regA] = memory.cell[PC++];
+    }
+    
+    private void store (int regA, int regB){
+        memory.cell[reg[regA]] = reg[regB];
+    }
+    /**********************arithmetic***********************/
+    /**
+     * adds two registers together, and stores in regA
+     * 
+     * @param regA register index that wants to be added
+     * @param regB  register index (value does not get deleted)
+     */
+    private void add (int regA, int regB){
+        reg[regA] = reg[regA] + reg[regB];
+    }
+    /**
+     * mutliplies two registers together and stores value in regA
+     * @param regA register index that wants to be multiplied
+     * @param regB register index that wants to be multiplied to
+     */
+    private void mul (int regA, int regB){
+        reg[regA] = reg[regA] * reg[regB];
+    }
+    /**
+     * subtracts the value of register a and register b, 
+     * stores difference in register A
+     * @param regA minuend 
+     * @param regB  subtrahend
+     */
+    private void sub (int regA, int regB){
+        reg[regA] = reg[regA] - reg[regB];
+    }
+    /**
+     * Divides register A and register B, stores value in 
+     * the index of registerA
+     * @param regA the dividend (numerator)
+     * @param regB the divisor (demoniator)
+     * @throws Exception regB cannot be zero; cannot divide by a zero
+     */
+    private void div (int regA, int regB) throws Exception{
+        if (regB ==0)
+                throw new Exception("cannot divide by zero");
+        reg[regA] = reg[regA] / reg[regB];
+    }
+    /****************************************logic**************************/
+    /**
+     * compares regA and regB whereas 1 is true, and 0 is false.
+     * the boolean expression is and; regA and regB ;stores the 
+     * outcome in the index of register at regA
+     * @param regA the index of the register you want to compare 
+     * @param regB the index of the register you want to compare
+     */
+    private void and (int regA, int regB){
+        if (reg[regA] != 0 && reg[regB] !=0)
+            reg[regA] = 1;
+        else 
+            reg[regA] = 0;
+        
+    }
+    
+    /**
+     * preforms the boolean operation or. whereas 1 is true, and 0 is false.
+     * stores the outcome in regA
+     * @param regA index of the register you want to compare
+     * @param regB index of the register you want to compare
+     */
+    private void or (int regA, int regB){
+        if (reg[regA] !=0 || reg[regB] != 0)
+            reg[regA] = 1;
+        else 
+            reg[regA] = 0;
+    }
+    
+    /**
+     * preforms the boolean operation not whereas 1 is ture, and 0 is false
+     * preforms the boolean operation not on regB
+     * stores the outcome in regA
+     * @param regA the register you would like to store the outcome of not (regB)
+     * @param regB the register you want to apply the boolean operation not
+     */
+    private void not (int regA, int regB){
+        if (reg[regB] !=0)
+            reg[regA] = 0;
+        else 
+            reg[regA] = 1;
+    }
+    
+    /********************************bitwise************************/
+    private void lshift (int regA, int regB){
+        reg[regA] = reg[regB] << 1;
+        
+    }
+    
+    private void rshift (int regA, int regB){
+        reg[regA] = reg[regB] >>1;
+    }
+   
+    private void bwc (int regA, int regB){
+        reg[regA] = reg[regA] & reg[regB];
+    }
+    
+    private void bwd (int regA, int regB){
+        reg[regA] = reg[regA] | reg[regB];
+    }
+    /**********************sequence control**************/
+    
+    
+    public void sequenceIf (int regA, int regB){
+    if (reg[regA] != 0)
+        PC = reg[regB];
+}
+    public void halt(){
+        System.out.println("program stopped.");
+    }
+    private void runCommand(int command) throws Exception{
+      String hex =  Integer.toString(command);
+      //takes hte last three numbers
+      int commandNumber = Integer.parseInt(hex.substring(7, 8));
+      int regA = Integer.parseInt(hex.substring(8, 9));
+      int regB = Integer.parseInt(hex.substring(9, 10));
+      
+      //examines which command to run,
+      //then executes the command.
+        switch(commandNumber){
+            case 1:
+                load(regA, regB);
+                break;
+            case 2:
+                loadc(regA);
+                break;
+            case 3:
+                store(regA, regB);
+                break;
+            case 4:
+                add(regA, regB);
+                break;
+            case 5:
+                mul(regA, regB);
+                break;
+            case 6:
+                sub(regA, regB);
+                break;
+            case 7:
+                div(regA, regB);
+                break;
+            case 8:
+                and (regA, regB);
+                break;
+            case 9:
+                or (regA, regB);
+                break;
+            case 10:
+                not (regA, regB);
+                break;
+            case 11:
+                lshift(regA, regB);
+                break;
+            case 12:
+                rshift(regA, regB);
+                break;
+            case 13:
+                bwc(regA, regB);
+                break;
+            case 14:
+                bwd(regA, regB);
+                break;
+            case 15:
+                sequenceIf(regA, regB);
+                break;
+            case 
+                
+        }
         /*
         Data Control
-1. load a b // reg[a] = cell[reg[b]]
-2. loadc a // reg[a] = cell[PC++]
-3. store a b // cell[reg[a]] = reg[b]
-Arithmetic
-4. add a b // reg[a] = reg[a] + reg[b]
-5. mul a b // reg[a] = reg[a] * reg[b]
-6. sub a b // reg[a] = reg[a] - reg[b]
-7. div a b // reg[a] = reg[a] / reg[b], error if reg[b] == 0
-Logic
-8. and a b // if (reg[a]!= 0&&reg[b]!=0) reg[a]=1 else reg[a]=0
-9. or a b // if (reg[a]!=0||reg[b]!=0) reg[a]=1 else reg[a]=0
 10. not a b // if (reg[b]!=0) reg[a]=0 else reg[a]=1
 Bitwise
 11. lshift a b // reg[a] = reg[b] << 1
